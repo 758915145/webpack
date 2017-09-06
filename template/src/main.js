5ef4ab8acaea9e39df79a42b3352eba94127d5b1
758915145 <758915145@qq.com>
2017-09-06 10:58:42 +0800

    Update main.js

--- template/src/main.js
+++ template/src/main.js
@@ -4,9 +4,12 @@ import 'babel-polyfill'
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Router from "vue-router";
+{{#router}}
+import Router from "vue-router"
+Vue.use(Router)
+{{/router}}
+
 import Vuex from "vuex";
-Vue.use(Router);
 Vue.use(Vuex);
 
 Vue.config.productionTip = false;
@@ -53,30 +56,32 @@ Vue.component('my-item-zh', {
 
 window.app = new Vue({
     el: '#app',
+    {{#router}}
     router:new Router({
         // mode: 'history',
         routes: (r=>{
             return r.keys().map(key=>{
                 var item = r(key);
-return {
-    path:item.name==='index'?`/`:`/${item.name}`,
-    name:item.name,
-    component:item
-}
-});
-})(require.context('./pages/',true,/index\.vue$/))
-}),
+                return {
+                    path:item.name==='index'?`/`:`/${item.name}`,
+                    name:item.name,
+                    component:item
+                }
+            });
+        })(require.context('./pages/',true,/index\.vue$/))
+    }),
+    {{/router}}
 
-template: '<App/>',
+    template: '<App/>',
     store:new Vuex.Store({
-    modules: (r=>{
-        let result = {};
-r.keys().forEach(key=>{
-    let item = r(key).default;//export defult
-result[item.name] = item;
-});
-return result;
-})(require.context('./',true,/store\.js$/))
-}),//Vuex
-components: {App: require('./App.vue')}
+        modules: (r=>{
+            let result = {};
+            r.keys().forEach(key=>{
+                let item = r(key).default;//export defult
+                result[item.name] = item;
+            });
+            return result;
+        })(require.context('./',true,/store\.js$/))
+    }),//Vuex
+    components: {App: require('./App.vue')}
 });
